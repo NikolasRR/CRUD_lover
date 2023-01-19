d6887d308aa86c14efa817ccee71e9096d373d2f
Nikolas <nkls99@gmail.com>
2023-01-19 20:57:28 -0300

    done

--- src/main/java/com/cars_api/api/DTOs/CarDTO.java
+++ src/main/java/com/cars_api/api/DTOs/CarDTO.java
@@ -1,4 +1,14 @@
 package com.cars_api.api.DTOs;
 
-public record CarDTO(String modelo, String fabricante, String dataFabricacao, Integer valor, Integer anoModelo) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record CarDTO(
+  @NotBlank String modelo, 
+  @NotBlank String fabricante, 
+  @NotBlank @Pattern(regexp = "^(0[1-9]|1[012])/(0[1-9]|[12][0-9]|[3][01])/\\d{4}$") String dataFabricacao, 
+  @NotNull Integer valor, 
+  @NotNull Integer anoModelo
+  ) {
 }
--- src/main/java/com/cars_api/api/controllers/CarsController.java
+++ src/main/java/com/cars_api/api/controllers/CarsController.java
@@ -3,8 +3,11 @@ package com.cars_api.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cars_api.api.DTOs.CarDTO;
 import com.cars_api.api.models.Car;
 import com.cars_api.api.repositories.CarRepo;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/cars")
@@ -26,7 +31,24 @@ public class CarsController {
   }
 
   @PostMapping
-  public void PostCar(@RequestBody CarDTO car) {
+  public void postCar(@RequestBody @Valid CarDTO car) {
     repo.save(new Car(car));
+  }
+
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Long id) {
+    repo.deleteById(id);
+  }
+
+  @PutMapping("/{id}")
+  public void update(@PathVariable Long id, @RequestBody @Valid CarDTO req) {
+    repo.findById(id).map(car -> {
+      car.setModelo(req.modelo());
+      car.setFabricante(req.fabricante());
+      car.setDataFabricacao(req.dataFabricacao());
+      car.setValor(req.valor());
+      car.setAnoModelo(req.anoModelo());
+      return repo.save(car);
+    });
   }
 }
